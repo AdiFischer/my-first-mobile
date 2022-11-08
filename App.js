a7e071306d6de3dc30a78216cd4659c0070cf4c6
AdiFischer <adifischer1@gmail.com>
2022-11-08 15:17:23 -0500

    First mobile

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Counter from './components/Counter';
+import Welcome from './components/Welcome';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Welcome firstName= {'Adi'} />
+      <Welcome firstName= {'Alon'} />
+      <Text style={styles.bigText}>This is soooooo cooooool🤩!!</Text>
+      <Counter />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  bigText: {
+  fontSize: '40vw',
+  color: 'white',
+  textAlign: 'center',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'darksalmon',
     alignItems: 'center',
     justifyContent: 'center',
   },
